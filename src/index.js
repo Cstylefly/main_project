188c4ae3dab713d5adb456d3fc119013b86cb891
tangcong <1432339230@qq.com>
2022-10-15 18:55:44 +0800

    自动化构建

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import {getCount} from './components/count'
 import './css/index.css'
 import './less/index.less'
 import './sass/index.scss'
-console.log(getCount([1,2,3]))
-
-const app = () => {
-    console.log('tag', '124')
-}
-
-const a = 10
-
-app()
 
 var chartDom = document.getElementById('echarts_div');
 var myChart = echarts.init(chartDom);
